2b3aa6cd1c210f3b9504df9b8aa1374aaae8a9f4
arissomantri <1006018@sttgarut.ac.id>
2015-06-29 14:19:59 +0700

    tambah berkas

---
+++ nilai_rata_uas/src/nilai_rata_uas/Nilai_rata_uas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilai_rata_uas;
+
+/**
+ *
+ * @author LUFTI
+ */
+public class Nilai_rata_uas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
